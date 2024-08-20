b18b9532ba058bed8a8bb548653b541b58e03191
Julio Bellini <42050835+juliobelli@users.noreply.github.com>
2024-08-20 01:15:04 -0300

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,8 +9,7 @@ function Home() {
         <div className="bg-indigo-900 flex justify-center">
             <div className="container grid grid-cols-2 text-white">
                 <div className="flex flex-col gap-4 items-center justify-center py-4">
-                    <h2 className="text-5xl font-bold">Sugestão</h2>
-                    <p className="text-xl">Expresse aqui sua opinião</p>
+                    <h2 className="text-5xl font-bold">Bem vindo!</h2>
 
                     <div className="flex justify-around gap-4">
                         <ModalPostagem />
