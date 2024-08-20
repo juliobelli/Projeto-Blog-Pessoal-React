9b1af52273fbb010b804cb7458a74b0a0ad3f3b1
Julio Bellini <42050835+juliobelli@users.noreply.github.com>
2024-08-20 01:24:48 -0300

    Update Perfil.tsx

--- src/pages/perfil/Perfil.tsx
+++ src/pages/perfil/Perfil.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../contexts/AuthContext'
-import loginLogo from '../../assets/pato.jpg'
+import loginLogo from '../../assets/gato.jpg'
 import { toastAlerta } from '../../utils/toastAlerta'
 function Perfil() {
 
@@ -20,7 +20,7 @@ function Perfil() {
     <div className='container mx-auto mt-4 rounded-2xl overflow-hidden'>
       <img className='w-full h-72 object-cover border-b-8 border-white' src={loginLogo} alt="Capa do Perfil" />
       <img src={usuario.foto} alt={`Foto de perfil de ${usuario.nome}`} className='text-white rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' />
-      <div className="relative mt-[-6rem] h-72 flex flex-col bg-fuchsia-700 text-white text-2xl items-center justify-center">
+      <div className="relative mt-[-6rem] h-72 flex flex-col bg-indigo-700 text-white text-2xl items-center justify-center">
         <p>Nome: {usuario.nome} </p>
         <p>Email: {usuario.usuario}</p>
       </div>
